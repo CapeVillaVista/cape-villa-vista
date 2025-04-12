62f13d40b065035be9c5a14b0f8b2db3ae16e5ae
CapeVillaVista <johnb@j2cm.com>
2025-04-12 09:50:15 -0400

    Delete App.js

--- App.js
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ fontFamily: "Arial, sans-serif", padding: "2rem" }}>
-      <h1>CapeVillaVista</h1>
-      <p>Modern Luxury Retreat on the Water in Cape Coral</p>
-    </div>
-  );
-}
