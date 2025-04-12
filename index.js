07b60eaa1e54dd8d6779156b5af35c1890dc2aee
CapeVillaVista <johnb@j2cm.com>
2025-04-12 09:48:58 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import CapeVillaVista from './CapeVillaVista';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<CapeVillaVista />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
